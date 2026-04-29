9158108bc9e4571f8858b7c51dc4fe16137d539e
seehowtronisliving <ethan.smith47@gmail.com>
2026-04-29 17:21:02 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const cacheName = 'form-v1';
-const assets = ['./', './index.html'];
+const CACHE_NAME = 'form-v1';
+const ASSETS = ['./index.html'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
