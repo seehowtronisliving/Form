bd8a870ed8fdfa1e2706b8cc2f155798fc5b3be1
seehowtronisliving <ethan.smith47@gmail.com>
2026-04-29 14:52:32 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
+const CACHE_NAME = 'vis-v2'; // Bump this number (v2, v3) every time you update
 self.addEventListener('install', (e) => {
- e.waitUntil(caches.open('v1').then((cache) => cache.addAll(['index.html', 'manifest.json'])));
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html', 'manifest.json'])));
+  self.skipWaiting(); // Forces the new version to take over immediately
 });
+
 self.addEventListener('fetch', (e) => {
- e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
