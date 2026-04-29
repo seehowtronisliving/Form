fac4cd1243ef03102981765117cd99ed4b49dde3
seehowtronisliving <ethan.smith47@gmail.com>
2026-04-29 16:11:57 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
-const CACHE_NAME = 'form-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'https://unpkg.com/lucide@latest'
-];
+const cacheName = 'form-v1';
+const assets = ['./', './index.html'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
