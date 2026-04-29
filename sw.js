8ffe76c4db636213d2cf2a1fbc7e23916c5bc109
seehowtronisliving <ethan.smith47@gmail.com>
2026-04-29 15:32:32 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,18 @@
-const CACHE_NAME = 'vis-v2'; // Bump this number (v2, v3) every time you update
+const CACHE_NAME = 'form-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://unpkg.com/lucide@latest'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html', 'manifest.json'])));
-  self.skipWaiting(); // Forces the new version to take over immediately
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
