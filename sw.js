7660ffcb5fa61c5afb0dd67ae2039fd4665ab960
seehowtronisliving <ethan.smith47@gmail.com>
2026-04-29 22:31:06 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
-const CACHE_NAME = 'form-v1';
-const ASSETS = ['./index.html'];
+const CACHE_NAME = 'strategic-strength-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://unpkg.com/lucide@latest'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
